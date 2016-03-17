746cff87e877a9c84fc386686f060e6550be8770
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2016-03-17 14:36:23 -0300

    Adiciona urls para o material da instrução

--- controller/InstructionCtrl.php
+++ controller/InstructionCtrl.php
@@ -2,6 +2,8 @@
 namespace BossEdu\Controller;
 
 use BossEdu\Model\InstructionQuery;
+use BossEdu\Model\MiMaterial;
+use BossEdu\Model\MiMaterialQuery;
 use BossEdu\Model\PersonQuery;
 use BossEdu\Model\PiLinkQuery;
 use BossEdu\Model\PresentationQuery;
@@ -157,6 +159,102 @@ class InstructionCtrl
             echo json_encode($participants);
         } else {
             throw new RestException(401, "Enrollment");
+        }
+    }
+
+    /**
+     * @noAuth
+     * @url OPTIONS /instruction/$instruction_id/material
+     */
+    public function optionsMaterial($instruction_id)
+    {
+        AuthCtrl::preFlightResponse();
+    }
+
+    /**
+     * @url POST /instruction/$instruction_id/material
+     */
+    public function newMaterial($instruction_id)
+    {
+        $instruction_id = urldecode($instruction_id);
+        $person = AuthCtrl::getSession()["id"];
+
+        if (PresentationCtrl::auth($instruction_id, $person, 2)) {
+            $file = $_FILES["file"];
+
+            if ($file["error"]) {
+                throw new RestException(410, "File error: ".$file["error"]);
+            }
+
+            $material = new MiMaterial();
+            $material->setFile(fopen($file["tmp_name"], "rb"));
+            $material->setName(basename($file["name"]));
+            $material->setMime($file["type"]);
+            $material->setInstructionId($instruction_id);
+            $material->save();
+        } else {
+            throw new RestException(401, "Unauthorized");
+        }
+    }
+
+    /**
+     * @noAuth
+     * @url OPTIONS /instruction/$instruction_id/material/$material_id
+     */
+    public function optionsGetMaterial($instruction_id, $material_id)
+    {
+        AuthCtrl::preFlightResponse();
+    }
+
+    /**
+     * @url GET /instruction/$instruction_id/material/$material_id
+     */
+    public function getMaterial($instruction_id, $material_id)
+    {
+        $instruction_id = urldecode($instruction_id);
+        $person = AuthCtrl::getSession()["id"];
+        $material_id = urldecode($material_id);
+
+        if (PresentationCtrl::auth($instruction_id, $person, 0)) {
+            $material_id = MiMaterialQuery::create()
+                ->findOneById($material_id);
+
+            if ($material_id) {
+                header("Content-Type: ".$material_id->getMime());
+                header("Content-Disposition: attachment; filename=\"".$material_id->getName()."\"");
+                header("Cache-Control: no-cache, no-store, must-revalidate");
+                header("Pragma: no-cache");
+                header("Expires: 0");
+                fpassthru($material_id->getFile());
+                exit(0);
+            } else {
+                throw new RestException(404, "Arquivo");
+            }
+        } else {
+            throw new RestException(401, "Unauthorized");
+        }
+    }
+
+    /**
+     * @url GET /instruction/$instruction_id/material
+     */
+    public function getMaterials($instruction_id)
+    {
+        $instruction_id = urldecode($instruction_id);
+        $person = AuthCtrl::getSession()["id"];
+
+        if (PresentationCtrl::auth($instruction_id, $person, 0)) {
+            $materials = MiMaterialQuery::create()
+                ->filterByPresentationId($instruction_id)
+                ->select(["Id", "Name", "Mime"])
+                ->find();
+
+            $materials = ["materials" =>
+                Util::adjustArrayCase($materials, "lower")
+            ];
+            echo json_encode($materials);
+        } else {
+            throw new RestException(401, "Unauthorized");
         }
     }
 }
