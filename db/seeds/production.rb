fabfbf00523c5c5feda80189a8e8c7ab745d097c
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2016-08-11 14:14:02 -0300

    Seed files per environment

---
+++ db/seeds/production.rb
@@ -0,0 +1,122 @@
+# Users
+# ======================================================================================================================
+silvana_affonso = User.create email: 'silaffonso@ifsp.edu.br', password: '030771'
+beatriz_maia = User.create email: 'beatriz.maia@email.com', password: '1601652'
+bruno_batista = User.create email: 'bruno.batista@email.com', password: '1601067'
+bruno_avelino = User.create email: 'bruno.avelino@email.com', password: '1600478'
+bruno_trevisan = User.create email: 'bruno.trevisan@email.com', password: '160077X'
+carolina_sartorelli = User.create email: 'carolina.sartorelli@email.com', password: '1201484'
+cendy_oliveira = User.create email: 'cendy.oliveira@email.com', password: '1601741'
+edipo_lohmann = User.create email: 'edipo.lohmann@email.com', password: '1600281'
+estevao_lourenco = User.create email: 'estevao.lourenco@email.com', password: '1600991'
+ezequiel_junior = User.create email: 'ezequiel.junior@email.com', password: '1301047'
+gabriel_silva = User.create email: 'gabriel.silva@email.com', password: '1600354'
+hiago_souza = User.create email: 'hiago.souza@email.com', password: '1600559'
+hugo_ciarrocchi = User.create email: 'hugo.ciarrocchi@email.com', password: '160046X'
+isabella_silva = User.create email: 'isabella.silva@email.com', password: '1504045'
+joel_ruivo = User.create email: 'joel.ruivo@email.com', password: '1600346'
+joyce_trindade = User.create email: 'joyce.trindade@email.com', password: '1600389'
+katio_pequeno = User.create email: 'katio.pequeno@email.com', password: '110246X'
+lucas_melo = User.create email: 'lucas.melo@email.com', password: '1402064'
+lucas_silva = User.create email: 'lucas.silva@email.com', password: '1000322'
+luciano_ferreira = User.create email: 'luciano.ferreira@email.com', password: '1600648'
+marcelo_oliveira = User.create email: 'marcelo.oliveira@email.com', password: '1401971'
+matheus_braz = User.create email: 'matheus.braz@email.com', password: '1102028'
+monica_pelicano = User.create email: 'monica.pelicano@email.com', password: '1600591'
+nayly_carvalho = User.create email: 'nayly.carvalho@email.com', password: '1301179'
+paulo_fios = User.create email: 'paulo.fios@email.com', password: '1601687'
+pedro_santos = User.create email: 'pedro.santos@email.com', password: '150357X'
+rafael_brito = User.create email: 'rafael.brito@email.com', password: '1601695'
+renato_aldrighi = User.create email: 'renato.aldrighi@email.com', password: '160032X'
+ricardo_carvalho = User.create email: 'ricardo.carvalho@email.com', password: '1600427'
+rodrigo_silva = User.create email: 'rodrigo.silva@email.com', password: '1601091'
+thon_ataide = User.create email: 'thon.ataide@email.com', password: '150102X'
+tiago_spana = User.create email: 'tiago.spana@email.com', password: '1301781'
+willian_correa = User.create email: 'willian.correa@email.com', password: '1401891'
+yudji_oliveira = User.create email: 'yudji.oliveira@email.com', password: '160161X'
+
+# People
+# ======================================================================================================================
+silvana_affonso = Person.create name: 'Silvana Maria Affonso de Lara', user: silvana_affonso
+beatriz_maia = Person.create name: 'Beatriz Marques Maia', user: beatriz_maia
+bruno_batista = Person.create name: 'Bruno da Silveira Batista', user: bruno_batista
+bruno_avelino = Person.create name: 'Bruno de Souza Avelino', user: bruno_avelino
+bruno_trevisan = Person.create name: 'Bruno Trevisan', user: bruno_trevisan
+carolina_sartorelli = Person.create name: 'Carolina Sartorelli', user: carolina_sartorelli
+cendy_oliveira = Person.create name: 'Cendy Andreoli de Oliveira', user: cendy_oliveira
+edipo_lohmann = Person.create name: 'Edipo Luiz Lohmann', user: edipo_lohmann
+estevao_lourenco = Person.create name: 'Estevao Ferreira Lourenco', user: estevao_lourenco
+ezequiel_junior = Person.create name: 'Ezequiel Sant Ana Junior', user: ezequiel_junior
+gabriel_silva = Person.create name: 'Gabriel Catice da Silva', user: gabriel_silva
+hiago_souza = Person.create name: 'Hiago Rafael de Souza', user: hiago_souza
+hugo_ciarrocchi = Person.create name: 'Hugo Saldanha Ciarrocchi', user: hugo_ciarrocchi
+isabella_silva = Person.create name: 'Isabella Cristina Gomes da Silva', user: isabella_silva
+joel_ruivo = Person.create name: 'Joel Rodrigues Ruivo', user: joel_ruivo
+joyce_trindade = Person.create name: 'Joyce Nathalia de Souza Trindade', user: joyce_trindade
+katio_pequeno = Person.create name: 'Katio de Almeida Pequeno', user: katio_pequeno
+lucas_melo = Person.create name: 'Lucas Ferreira Melo', user: lucas_melo
+lucas_silva = Person.create name: 'Lucas Marques da Silva', user: lucas_silva
+luciano_ferreira = Person.create name: 'Luciano Tochio Ferreira', user: luciano_ferreira
+marcelo_oliveira = Person.create name: 'Marcelo Gomes de Oliveira', user: marcelo_oliveira
+matheus_braz = Person.create name: 'Matheus Mendes Braz', user: matheus_braz
+monica_pelicano = Person.create name: 'Monica Lourenco Gomes Pelicano', user: monica_pelicano
+nayly_carvalho = Person.create name: 'Nayly Muniz Carvalho', user: nayly_carvalho
+paulo_fios = Person.create name: 'Paulo Henrique Fios', user: paulo_fios
+pedro_santos = Person.create name: 'Pedro Henrique Dos Santos', user: pedro_santos
+rafael_brito = Person.create name: 'Rafael de Campos Brito', user: rafael_brito
+renato_aldrighi = Person.create name: 'Renato Aldrighi', user: renato_aldrighi
+ricardo_carvalho = Person.create name: 'Ricardo de Carvalho', user: ricardo_carvalho
+rodrigo_silva = Person.create name: 'Rodrigo Hideo Tanaka da Silva', user: rodrigo_silva
+thon_ataide = Person.create name: 'Thon Christopher Ataide', user: thon_ataide
+tiago_spana = Person.create name: 'Tiago Spana', user: tiago_spana
+willian_correa = Person.create name: 'Willian Toshio Nogiri Correa', user: willian_correa
+yudji_oliveira = Person.create name: 'Yudji Nakaema Oliveira', user: yudji_oliveira
+
+# Events
+# ======================================================================================================================
+ads = Event.create code: 'ADS', name: 'Tecnologia em Análise e Desenvolvimento de Sistemas', workload: 2100.7
+
+# Lectures
+# ======================================================================================================================
+bd1 = Lecture.create code: 'BD1', name: 'Banco de Dados I', workload: 79.2
+
+# Instructions
+# ======================================================================================================================
+bd1_instruction = Instruction.create event: ads , lecture: bd1, class_number: 1, start_date: '2015-07-28', end_date: '2015-12-22'
+
+# Enrolls
+# ======================================================================================================================
+Enroll.create person: silvana_affonso, instruction: bd1_instruction, profile: 1
+Enroll.create person: beatriz_maia, instruction: bd1_instruction, profile: 0
+Enroll.create person: bruno_batista, instruction: bd1_instruction, profile: 0
+Enroll.create person: bruno_avelino, instruction: bd1_instruction, profile: 0
+Enroll.create person: bruno_trevisan, instruction: bd1_instruction, profile: 0
+Enroll.create person: carolina_sartorelli, instruction: bd1_instruction, profile: 0
+Enroll.create person: cendy_oliveira, instruction: bd1_instruction, profile: 0
+Enroll.create person: edipo_lohmann, instruction: bd1_instruction, profile: 0
+Enroll.create person: estevao_lourenco, instruction: bd1_instruction, profile: 0
+Enroll.create person: ezequiel_junior, instruction: bd1_instruction, profile: 0
+Enroll.create person: gabriel_silva, instruction: bd1_instruction, profile: 0
+Enroll.create person: hiago_souza, instruction: bd1_instruction, profile: 0
+Enroll.create person: hugo_ciarrocchi, instruction: bd1_instruction, profile: 0
+Enroll.create person: isabella_silva, instruction: bd1_instruction, profile: 0
+Enroll.create person: joel_ruivo, instruction: bd1_instruction, profile: 0
+Enroll.create person: joyce_trindade, instruction: bd1_instruction, profile: 0
+Enroll.create person: katio_pequeno, instruction: bd1_instruction, profile: 0
+Enroll.create person: lucas_melo, instruction: bd1_instruction, profile: 0
+Enroll.create person: lucas_silva, instruction: bd1_instruction, profile: 0
+Enroll.create person: luciano_ferreira, instruction: bd1_instruction, profile: 0
+Enroll.create person: marcelo_oliveira, instruction: bd1_instruction, profile: 0
+Enroll.create person: matheus_braz, instruction: bd1_instruction, profile: 0
+Enroll.create person: monica_pelicano, instruction: bd1_instruction, profile: 0
+Enroll.create person: nayly_carvalho, instruction: bd1_instruction, profile: 0
+Enroll.create person: paulo_fios, instruction: bd1_instruction, profile: 0
+Enroll.create person: pedro_santos, instruction: bd1_instruction, profile: 0
+Enroll.create person: rafael_brito, instruction: bd1_instruction, profile: 0
+Enroll.create person: renato_aldrighi, instruction: bd1_instruction, profile: 0
+Enroll.create person: ricardo_carvalho, instruction: bd1_instruction, profile: 0
+Enroll.create person: rodrigo_silva, instruction: bd1_instruction, profile: 0
+Enroll.create person: thon_ataide, instruction: bd1_instruction, profile: 0
+Enroll.create person: tiago_spana, instruction: bd1_instruction, profile: 0
+Enroll.create person: willian_correa, instruction: bd1_instruction, profile: 0
+Enroll.create person: yudji_oliveira, instruction: bd1_instruction, profile: 0
