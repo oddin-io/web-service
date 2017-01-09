44bd43cc78bbb3a0498d326734821082c25157c0
Heitor Kuba <hjkuba@yahoo.com.br>
2017-01-09 20:39:02 -0200

    Validates uniqueness of instruction class number

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -22,4 +22,5 @@ class Instruction < ApplicationRecord
 
   validates :class_number, :start_date, :end_date, presence: true
   validates :class_number, numericality: {only_integer: true}
+  validates :class_number, uniqueness: {scope: [:lecture, :event]}
 end
