bc17cd1f6548e34cb91d4cf672ccebd6b9ad8888
Everson <everson.zambon@gmail.com>
2017-10-13 02:23:58 -0300

    Setting up databases to tests

---
+++ db/migrate/20171013025527_create_test_questions.rb
@@ -0,0 +1,17 @@
+class CreateTestQuestions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :test_questions do |t|
+      t.integer :number, null: false
+      t.text :description, null: false
+      t.text :answer, null: true
+      t.decimal :value, null: false
+      t.boolean :kind, null: false
+      t.text :comment, null: true
+      
+      t.belongs_to :attachable, polymorphic: true, index: true
+      t.references :test, foreign_key: true, null: false
+
+      t.timestamps
+    end
+  end
+end
