13ea5f95c6d8e58e7d22e5ff1b205bbfe98a61e2
Everson <everson.zambon@gmail.com>
2017-10-22 03:51:42 -0200

    Add attribute => created_at

--- app/serializers/test_serializer.rb
+++ app/serializers/test_serializer.rb
@@ -1,5 +1,5 @@
 class TestSerializer < ActiveModel::Serializer
-  attributes :id, :title, :date_available, :available_at, :closes_at
+  attributes :id, :title, :created_at, :date_available, :available_at, :closes_at
   has_one :instruction
   has_one :person
 end
